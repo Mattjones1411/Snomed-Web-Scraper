910a8af7346d387b9a6feaf4eac5cca62628f02c
unknown <jonesmat@live.co.uk>
2021-06-07 01:30:16 +0100

    (Functioning, tidying up and docs still to do)

--- Snomed_Scrape.py
+++ Snomed_Scrape.py
@@ -10,51 +10,24 @@ import tqdm
 import os
 import csv
 
-term = 'Diabetes'
+decision = input("Would you like to use a CSV input? (Y/N): ")
+if decision.upper() == 'Y':
+    file_path = input("Please input the file path: ")
+    file = pd.read_csv(f'{file_path}.csv', encoding='latin1')
+    term_dataframe = pd.DataFrame(file.iloc[0:, 0:].values)
+    term = term_dataframe[0].tolist()
+else:
+    term = input("What term would you like to search?: ")
+
 driver = webdriver.Chrome()
 
-driver.get('https://termbrowser.nhs.uk/?')
-sleep(4)
-driver.find_element_by_id('accept-license-button-modal').click()
-driver.find_element_by_xpath('//*[@id="welcome-perspective"]/div/p[6]/a[1]').click()
-driver.find_element_by_xpath('//a[@href="#fh-search_canvas"]').click()
-
-WebDriverWait(driver, 10).until(
-    ec.visibility_of_element_located((By.XPATH, '//*[@id="fh-search_canvas-searchConfigBar"]/div[2]/button'))).click()
-driver.find_element_by_id('fh-search_canvas-activeInactiveButton').click()
-
-driver.find_element_by_xpath('//input[@id="fh-search_canvas-searchBox"]').send_keys(term)
-
-driver.find_element_by_id('fh-cd1_canvas-configButton').click()
-checkbox_one = WebDriverWait(driver, 10).until(
-    ec.visibility_of_element_located((By.ID, 'fh-cd1_canvas-displayIdsOption')))
-if not checkbox_one.is_selected():
-    checkbox_one.click()
-
-driver.find_element_by_id('fh-cd1_canvas-apply-button').click()
-
-checkbox_two = WebDriverWait(driver, 10).until(
-    ec.visibility_of_element_located((By.ID, 'fh-search_canvas-groupConcept')))
-if not checkbox_two.is_selected():
-    checkbox_two.click()
-
-while True:
-    try:
-        men_menu = WebDriverWait(driver, 10).until(
-            ec.visibility_of_element_located((By.ID, 'fh-search_canvas-more')))
-        ActionChains(driver).move_to_element(men_menu).perform()
-        men_menu.click()
-    except TimeoutException:
-        break
-
 if type(term) == list:
+
     file_name = input('What would you like the file to be/is the file called? ')
-    table = driver.find_element_by_id('fh-search_canvas-resultsTable')
-    results = table.find_elements(By.TAG_NAME, "tr")
     output = []
     start_index = 0
     internal_index = 0
-    csv_file_name = f'{file_name}.csv'
+    file = f'{file_name}.csv'
     write_mode = 'w'
 
     if os.path.isfile(f'{file_name}.csv'):
@@ -64,51 +37,113 @@ if type(term) == list:
         internal_index = df.iat[-1, 6]
         write_mode = 'a'
 
-    with tqdm.tqdm(total=len(term) - start_index - 1) as pbar, open(csv_file_name, write_mode, newline='') as file:
+    with tqdm.tqdm(total=len(term) - start_index - 1) as pbar, open(file_name, write_mode, newline='') as file:
         writer = csv.writer(file)
-        for item in term[start_index:-1]:
-            for result in results[internal_index]:
-                result.find_element_by_xpath('.//td[1]/div/a').click()
-                while True:
-                    try:
-                        concept_id = result.find_element_by_xpath('.//td[1]/div').get_attribute('data-concept-id')
-                        details = WebDriverWait(driver, 10).until(
-                            ec.visibility_of_element_located((By.XPATH, '//*[@href="#details-fh-cd1_canvas"]')))
-                        ActionChains(driver).move_to_element(details).perform()
-                        details.click()
-                        while True:
-                            try:
-                                result_pane = WebDriverWait(driver, 10).until(
-                                    ec.visibility_of_element_located(
-                                        (By.XPATH, '//*[@id="fh-cd1_canvas-descriptions-panel"]')))
-                                result_tables = result_pane.find_elements(By.TAG_NAME, "table")
-                                for table in result_tables:
-                                    rows = table.find_elements_by_xpath('.//tbody/tr')
-                                    for row in rows:
-                                        description_name = row.find_element_by_xpath('.//td[1]').text.replace('&nbsp;',
-                                                                                                              '')[
-                                                           4:].lstrip()
-                                        description_id = row.find_element_by_xpath('.//td[2]').text
-                                        preferred = row.find_element_by_xpath('.//td[3]').text.replace('&nbsp;',
-                                                                                                       '').lstrip()
-                                        index = term.index(item)
-                                        item_index = results.index(result)
-                                        output = [concept_id, description_name, description_id, preferred, index, term,
-                                                  item_index]
-                                        writer.writerow(output)
-                                break
-                            except TimeoutException:
-                                sleep(60)
-                                details.click()
-                        break
-                    except TimeoutException:
-                        sleep(60)
-                        result.find_element_by_xpath('.//td[1]/div/a').click()
+        driver.get('https://termbrowser.nhs.uk/?')
+        sleep(4)
+        driver.find_element_by_xpath('//*[@id="accept-license-button-modal"]').click()
+        driver.find_element_by_xpath('//*[@id="welcome-perspective"]/div/p[6]/a[1]').click()
+        driver.find_element_by_xpath('//a[@href="#fh-search_canvas"]').click()
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located(
+                (By.XPATH, '//*[@id="fh-search_canvas-searchConfigBar"]/div[2]/button'))).click()
+        driver.find_element_by_id('fh-search_canvas-activeInactiveButton').click()
+        driver.find_element_by_id('fh-cd1_canvas-configButton').click()
+        checkbox_one = WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.ID, 'fh-cd1_canvas-displayIdsOption')))
+        if not checkbox_one.is_selected():
+            checkbox_one.click()
+        checkbox_two = WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.ID, 'fh-search_canvas-groupConcept')))
+        driver.find_element_by_id('fh-cd1_canvas-apply-button').click()
+        if not checkbox_two.is_selected():
+            checkbox_two.click()
+
+        for item in term:
+            driver.find_element_by_xpath('//input[@id="fh-search_canvas-searchBox"]').clear()
+            driver.find_element_by_xpath('//input[@id="fh-search_canvas-searchBox"]').send_keys(str(item))
+            while True:
+                try:
+                    men_menu = WebDriverWait(driver, 10).until(
+                        ec.visibility_of_element_located((By.ID, 'fh-search_canvas-more')))
+                    ActionChains(driver).move_to_element(men_menu).perform()
+                    men_menu.click()
+                except TimeoutException:
+                    break
+            table = driver.find_element_by_id('fh-search_canvas-resultsTable')
+            result = table.find_element_by_xpath('//*[@id="fh-search_canvas-resultsTable"]/tr[1]')
+            result.click()
+            while True:
+                try:
+                    concept_id = result.find_element_by_xpath('.//td[1]/div').get_attribute('data-concept-id')
+                    details = WebDriverWait(driver, 10).until(
+                        ec.visibility_of_element_located((By.XPATH, '//*[@href="#details-fh-cd1_canvas"]')))
+                    ActionChains(driver).move_to_element(details).perform()
+                    details.click()
+                    while True:
+                        try:
+                            result_pane = WebDriverWait(driver, 10).until(
+                                ec.visibility_of_element_located(
+                                    (By.XPATH, '//*[@id="fh-cd1_canvas-descriptions-panel"]')))
+                            result_tables = result_pane.find_elements(By.TAG_NAME, "table")
+                            for table in result_tables:
+                                rows = table.find_elements_by_xpath('.//tbody/tr')
+                                for row in rows:
+                                    description_name = row.find_element_by_xpath('.//td[1]').text.replace(
+                                                                                '&nbsp;', '')[4:].lstrip()
+                                    description_id = row.find_element_by_xpath('.//td[2]').text
+                                    preferred = row.find_element_by_xpath('.//td[3]').text.replace('&nbsp;',
+                                                                                                   '').lstrip()
+                                    index = term.index(item)
+                                    output = [concept_id, description_id, description_name, preferred, index]
+                                    writer.writerow(output)
+                            break
+                        except TimeoutException:
+                            sleep(60)
+                            details.click()
+                    break
+                except TimeoutException:
+                    sleep(60)
+                    result.find_element_by_xpath('.//td[1]/div/a').click()
             pbar.update()
-        driver.close()
-        file.close()
+    driver.close()
+    file.close()
 
 else:
+    driver.get('https://termbrowser.nhs.uk/?')
+    sleep(4)
+    if not driver.find_elements_by_id("accept-license-button-modal").isEmpty():
+        driver.find_element_by_id('accept-license-button-modal').click()
+    driver.find_element_by_xpath('//*[@id="welcome-perspective"]/div/p[6]/a[1]').click()
+    driver.find_element_by_xpath('//a[@href="#fh-search_canvas"]').click()
+
+    WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located(
+            (By.XPATH, '//*[@id="fh-search_canvas-searchConfigBar"]/div[2]/button'))).click()
+    driver.find_element_by_id('fh-search_canvas-activeInactiveButton').click()
+    driver.find_element_by_xpath('//input[@id="fh-search_canvas-searchBox"]').send_keys(term)
+    driver.find_element_by_id('fh-cd1_canvas-configButton').click()
+    checkbox_one = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.ID, 'fh-cd1_canvas-displayIdsOption')))
+    if not checkbox_one.is_selected():
+        checkbox_one.click()
+
+    driver.find_element_by_id('fh-cd1_canvas-apply-button').click()
+
+    checkbox_two = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.ID, 'fh-search_canvas-groupConcept')))
+    if not checkbox_two.is_selected():
+        checkbox_two.click()
+
+    while True:
+        try:
+            men_menu = WebDriverWait(driver, 10).until(
+                ec.visibility_of_element_located((By.ID, 'fh-search_canvas-more')))
+            ActionChains(driver).move_to_element(men_menu).perform()
+            men_menu.click()
+        except TimeoutException:
+            break
+
     table = driver.find_element_by_id('fh-search_canvas-resultsTable')
     results = table.find_elements(By.TAG_NAME, "tr")
     output = []
@@ -159,5 +194,5 @@ else:
                     sleep(60)
                     result.find_element_by_xpath('.//td[1]/div/a').click()
             pbar.update()
-        driver.close()
-        file.close()
+    driver.close()
+    file.close()
